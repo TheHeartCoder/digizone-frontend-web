77da5f302585228856483374e61057985426e412
Arindam Paul <arindam.paul@sworks.io>
2022-07-25 23:17:00 +0530

    WIP

---
+++ digizone-app-frontend/components/shared/Heading.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Heading = () => {
+	return (
+		<Head>
+			<title>DigiZone</title>
+			<meta
+				name='description'
+				content='DigiZone - Get Instant License In a Click'
+			/>
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
+	);
+};
+
+export default Heading;
